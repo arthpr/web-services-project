b507c2f3ac0959ce9e782e9c032004b0ed2114e7
Arthur Rosendo <arthurrosendo272@gmail.com>
2025-02-13 16:44:53 -0300

    updates

--- src/main/java/com/arthpr/coursejava/repositories/OrderItemRepository.java
+++ src/main/java/com/arthpr/coursejava/repositories/OrderItemRepository.java
@@ -3,7 +3,8 @@ package com.arthpr.coursejava.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.arthpr.coursejava.entities.OrderItem;
+import com.arthpr.coursejava.entities.pk.OrderItemPK;
 
-public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+public interface OrderItemRepository extends JpaRepository<OrderItem, OrderItemPK> {
 
 }
