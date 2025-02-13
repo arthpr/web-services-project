caaf2fed043072bc9e65b0e92ac35a65f0fd32ec
Arthur Rosendo <arthurrosendo272@gmail.com>
2025-02-13 10:52:57 -0300

    Exception handling - findById

--- src/main/java/com/arthpr/coursejava/services/UserService.java
+++ src/main/java/com/arthpr/coursejava/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.arthpr.coursejava.entities.User;
 import com.arthpr.coursejava.repositories.UserRepository;
+import com.arthpr.coursejava.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = urep.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
@@ -30,5 +31,17 @@ public class UserService {
 	
 	public void delete(Long id) {
 		urep.deleteById(id);
+	}
+	
+	public User update(Long id, User obj) {
+		User entity = urep.getOne(id);
+		updateData(entity, obj);
+		return urep.save(entity);
+	}
+
+	private void updateData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
 	}
 }
