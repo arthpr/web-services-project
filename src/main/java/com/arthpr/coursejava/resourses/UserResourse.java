eca21cb0fae55b68a6f119b465482ce101049833
Arthur Rosendo <arthurrosendo272@gmail.com>
2025-02-13 10:26:44 -0300

    User update

--- src/main/java/com/arthpr/coursejava/resourses/UserResourse.java
+++ src/main/java/com/arthpr/coursejava/resourses/UserResourse.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -44,9 +45,15 @@ public class UserResourse {
 		return ResponseEntity.created(uri).body(obj);
 	}
 	
-	@DeleteMapping
+	@DeleteMapping(value = "/{id}")
 	public ResponseEntity<Void> delete(@PathVariable Long id){
 		usv.delete(id);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User obj) {
+		obj = usv.update(id, obj);
+		return ResponseEntity.ok().body(obj);
 	}
 }
